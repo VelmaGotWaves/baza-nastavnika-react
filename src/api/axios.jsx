75e8d1ae549fe262f38adabaf5b9ffd0c2d80967
VelmaGotWaves <ognjen.mobilni2407@gmial.com>
2023-05-07 23:46:39 +0200

    fsdfsd

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://baza-nastavnika-api.onrender.com';
+// const BASE_URL = 'http://localhost:3500';
+
 
 export default axios.create({
     baseURL: BASE_URL
